141b0518d94c01349710b860fcbbb75acbd30556
yangwuhsu <fuhui1104ttw@gmail.com>
2019-06-27 18:23:31 +0800

    完成九成功能

--- cart.js
+++ cart.js
@@ -1,23 +1,30 @@
- 
+    
+    // 查詢CheckOut按鈕
+    const checkOut = document.querySelector('.cart_check_out_container');
+
     // 顯示畫面的容器
     const cartDOM = document.querySelector('.cart_container');
     // 查詢顯示結帳處
     const tatalBtn = document.querySelector('.cart_total_price');
 
-    // localstorage 被點擊的商品
+    // 取得localstorage 被點擊的商品
     const cartContents = JSON.parse(localStorage.getItem('mainItems'));
-    console.log(cartContents);
-    
-    // 購物車數量
-    const cartCount = document.querySelector('.cart_count');
 
-    if(cartContents !== null){
-        cartCount.textContent = cartContents.length;
+    // 取得商品介紹頁被點擊的商品
+    const productItems = JSON.parse(localStorage.getItem('productItems'));
+    console.log(productItems);
+
+    // 如果商品介紹有值 加入主要localstorage裡
+    if(productItems !== null){
+        cartContents.push(productItems);
     }
-    
-    
+    // 總金額預設為零
     totalPrice = 0;
-    if(cartContents !== null){
+
+    // 購物車數字
+    const cartCount = document.querySelector('.cart_count');
+    if(cartContents.length > 0 ){
+        cartCount.textContent = cartContents.length;
         cartContents.forEach(cartContent => {
             // 去掉錢符號取出價錢
             const subCartPrice = cartContent.price.substring(1);
@@ -39,6 +46,13 @@
             );
         });
     }
+
+    checkOut.addEventListener('click',()=>{
+        localStorage.clear();
+    })
+    
+  
+
 
  
 
--- product.js
+++ product.js
@@ -1,45 +1,49 @@
- //  讀取localStorage資料
- const mainCarts = JSON.parse(localStorage.getItem('mainItems'));
-        
- // 查詢加到購物車按鈕
- const productAddtoCartBtn = document.querySelector('[data-action = "proAddCartBtn"]');
- 
- // 查詢購物車數量
- const productCartCount = document.querySelector('.cart_count');
- if(mainCarts !== null){
-     productCartCount.textContent = mainCarts.length;
- }else{
-     productCartCount.textContent = 0;
- }
- // 查詢商品介紹dom
- const productContainer = document.querySelector('.pro-content');
- 
- // 抓圖片層dom
- const productImage = document.querySelector('.pro-pic');
- 
- // 建立按鈕監聽事件
- productAddtoCartBtn.addEventListener('click',productAddtoCart);
+    //  讀取localStorage資料
+    const mainCarts = JSON.parse(localStorage.getItem('mainItems'));
+            
+    // 查詢加到購物車按鈕
+    const productAddtoCartBtn = document.querySelector('[data-action = "proAddCartBtn"]');
+    
+    // 查詢購物車數量
+    const productCartCount = document.querySelector('.cart_count');
+   
+    // 查詢商品介紹dom
+    const productContainer = document.querySelector('.pro-content');
+    
+    // 抓圖片層dom
+    const productImage = document.querySelector('.pro-pic');
+    
+    // 建立按鈕監聽事件
+    productAddtoCartBtn.addEventListener('click',productAddtoCart);
 
- // 存放本頁商品資訊
- const productIntro = {
-         image: productImage.querySelector('img').src,
-         name: productContainer.querySelector('.pro-title').textContent,
-         price: productContainer.querySelector('.pro-price').textContent
-     }
+    // 存放本頁商品資訊
+    const productIntro = {
+            image: productImage.querySelector('img').src,
+            name: productContainer.querySelector('.pro-title').textContent,
+            price: productContainer.querySelector('.pro-price').textContent
+        }
 
- //  比對已在購物車商品是否和本頁商品重複
- if(mainCarts !== null){
-         mainCarts.forEach(mainCart => {
-         if(mainCart.name === productIntro.name){
-             productAddtoCartBtn.value = 'In Cart';
-             productAddtoCartBtn.classList.add('inputInCart');
-         }
-     });   
- }
- function productAddtoCart(){
-     if( productAddtoCartBtn.value !== 'In Cart'){
-         localStorage.setItem('productItems',JSON.stringify(productIntro));
-         productAddtoCartBtn.value = 'In Cart';
-         productAddtoCartBtn.classList.add('inputInCart');
-     }
- }
+    // 比對已在購物車商品是否和本頁商品重複
+    if(mainCarts !== null){
+        productCartCount.textContent = mainCarts.length;
+            mainCarts.forEach(mainCart => {
+            if(mainCart.name === productIntro.name){
+                productAddtoCartBtn.value = 'In Cart';
+                productAddtoCartBtn.classList.add('inputInCart');
+            }
+        });   
+    }else{
+        productCartCount.textContent = 0;
+    }
+
+    function productAddtoCart(){
+        if( productAddtoCartBtn.value !== 'In Cart'){
+            productCartCount.textContent = '1';
+            localStorage.setItem('productItems',JSON.stringify(productIntro));
+            productAddtoCartBtn.value = 'In Cart';
+            productAddtoCartBtn.classList.add('inputInCart');
+            if(mainCarts.length >0 ){
+                productCartCount.textContent = mainCarts.length+1;
+            }
+        }
+    }
